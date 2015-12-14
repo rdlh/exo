0e55c24ed2ab53bfe3ec20c3b5600671656b0dc7
Rémi Delhaye <contact@rdlh.io>
2015-12-14 17:07:06 +0100

    v0.1

---
+++ main.js
@@ -0,0 +1,8 @@
+$('nav a').on('click', function() {
+  scrollToAnchor('id3');
+})
+
+function scrollToAnchor(aid) {
+  var aTag = $("a[name='"+ aid +"']");
+  $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
